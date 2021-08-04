71a24ca00e354bba982fe221d8d47d58bccc8c64
Miguel <axtra2766@gmail.com>
2021-08-03 23:27:44 -0400

    Primeros cambios

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const {src, dest, watch, series, parallel} = require('gulp');
+var sass = require('gulp-sass') (require('sass'));
+const paths = {
+     scss: 'src/scss/**/*scss'
+}
+function cssMinificado(){
+    return src(paths.scss)
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }) )
+        .pipe(dest('./build/css'))
+}
+function css(){
+    return src(paths.scss)
+    .pipe( sass({
+        outputStyle: 'expanded'
+    }) )
+    .pipe( dest('./build/css'))
+}
+function watchArchivos(){
+    watch(paths.scss, css);
+}
+exports.cssMinificado = cssMinificado;
+exports.watchArchivos = watchArchivos;
